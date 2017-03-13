a8c0f7c9ba9958f2db3d3fdac4121c53ded0e48d
Blake Lawler <blake.lawler@rrenovations.com>
2017-03-13 14:14:46 -0400

    Now I'm on the right branch.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,7 +9,11 @@ public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+        List<String> ddlSourceCollection = new List<String> { "Item1", "Item2", "Item3" };
+        ListItem defaultListItem = new ListItem("-- Select item --");
+        ddlTest.DataSource = ddlSourceCollection;
+        ddlTest.DataBind();
+        ddlTest.Items.Insert(0, defaultListItem);
     }
 
 
